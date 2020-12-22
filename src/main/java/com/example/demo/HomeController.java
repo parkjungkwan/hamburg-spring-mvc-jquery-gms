43cb6099bd82a00b01ed8fb68a49799954ef7948
parkjungkwan <pakjkwan@gmail.com>
2020-12-22 11:09:12 +0900

    11

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -10,26 +10,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		
-		logger.info("======== Welcome home! =========");
-		
-		
-		
-		return "home";
-	}
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/")
+    public String index() {
+        logger.info("Hello. This is LogManager's logger");
+        return "index";
+    }
+
+    @GetMapping("/move/{dir}/{page}")
+    public String move(@PathVariable String dir, @PathVariable String page) {
+        logger.info("이동 디렉토리: " + dir);
+        logger.info("이동 파일: " + page);
+        return String.format("%s/%s", dir, page);
+    }
+
+    @GetMapping("/transfer/{dir}/{sub}/{page}")
+    public String transfer(@PathVariable String dir, @PathVariable String sub, @PathVariable String page) {
+        logger.info("이동 디렉토리: " + dir);
+        logger.info("이동 서브 디렉토리: " + sub);
+        logger.info("이동 파일: " + page);
+        return String.format("%s/%s/%s", dir, sub, page);
+    }
 	
 }
